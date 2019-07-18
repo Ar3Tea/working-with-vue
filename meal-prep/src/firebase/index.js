4e33693acd005b7ceb841e257024704efae7abf0
AreTea <tbusinessr@gmail.com>
2019-07-18 17:24:25 -0400

    getting confused

---
+++ meal-prep/src/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUmhVMR6jrF6a_ct1ZzF8uQ6xDMI5pGvU",
+    authDomain: "meal-prep-992b2.firebaseapp.com",
+    databaseURL: "https://meal-prep-992b2.firebaseio.com",
+    projectId: "meal-prep-992b2",
+    storageBucket: "",
+    messagingSenderId: "283707562750",
+    appId: "1:283707562750:web:d3f8575c93e1cafc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- meal-prep/src/store.js
+++ meal-prep/src/store.js
@@ -1,10 +1,64 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import firebase from 'firebase';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: 'c094e562',
+                        app_key: '024375fe2126d38297ff33637d8381f6',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        },
+        userLogin({ commit }, { email, password }) {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+                .then(user => {
+                    commit('setUser', user);
+                    commit('setIsAuthenticated', true);
+                })
+                .catch(() => {
+                    commit('setUser', null);
+                    commit('setIsAuthenticated', false);
+                });
+        },
+        userSignOut({ commit }) {
+            firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                    commit('setUser', null);
+                    commit('setIsAuthenticated', false);
+                    router.push('/');
+                })
+                .catch(() => {
+                    commit('setUser', null);
+                    commit('setIsAuthenticated', false);
+                    router.push('/');
+                });
+        }
+    }
 });
